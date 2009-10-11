9b622d29549954b7bdb7c4992cb1a140794de93d
Jon Shier <jon@jonshier.com>
2009-10-11 08:53:21 -0400

    Eliminate parts of CTGradient we don't use. Elimite CTGradientView entirely.

--- Other Sources/Foreign Code/CTGradient/CTGradient.h
+++ Other Sources/Foreign Code/CTGradient/CTGradient.h
@@ -36,38 +36,9 @@ typedef enum  _CTBlendingMode
 
 + (id)gradientWithBeginningColor:(NSColor *)begin endingColor:(NSColor *)end;
 
-+ (id)aquaSelectedGradient;
 + (id)aquaNormalGradient;
-+ (id)aquaPressedGradient;
 
-+ (id)unifiedSelectedGradient;
-+ (id)unifiedNormalGradient;
-+ (id)unifiedPressedGradient;
-+ (id)unifiedDarkGradient;
-
-+ (id)sourceListSelectedGradient;
-+ (id)sourceListUnselectedGradient;
-
-+ (id)rainbowGradient;
-+ (id)hydrogenSpectrumGradient;
-
-- (CTGradient *)gradientWithAlphaComponent:(float)alpha;
-
-- (CTGradient *)addColorStop:(NSColor *)color atPosition:(float)position;	//positions given relative to [0,1]
-- (CTGradient *)removeColorStopAtIndex:(unsigned)index;
-- (CTGradient *)removeColorStopAtPosition:(float)position;
-
-- (CTGradientBlendingMode)blendingMode;
-- (NSColor *)colorStopAtIndex:(unsigned)index;
-- (NSColor *)colorAtPosition:(float)position;
-
-
-- (void)drawSwatchInRect:(NSRect)rect;
 - (void)fillRect:(NSRect)rect angle:(float)angle;					//fills rect with axial gradient
-																	//	angle in degrees
-- (void)radialFillRect:(NSRect)rect;								//fills rect with radial gradient
-																	//  gradient from center outwards
-- (void)fillBezierPath:(NSBezierPath *)path angle:(float)angle;
-- (void)radialFillBezierPath:(NSBezierPath *)path;
+
 
 @end
